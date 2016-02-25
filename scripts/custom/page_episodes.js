4b4a9519521a93194c1213cbf080cd3fe7635b78
Max <max@wild.as>
2016-02-25 13:34:41 +0100

    episodes poster parallax

--- scripts/custom/page_episodes.js
+++ scripts/custom/page_episodes.js
@@ -13,28 +13,28 @@ SQSP.functions.initPage = function() {
         });
     });
 
-    //SQSP.$objects.postersContainer = jQuery('.posters');
-    //SQSP.$objects.postersItems = jQuery('.prllxposter');
-    //SQSP.instances.ParallaxPosters = [];
-    //
-    //// CREATE INTERACTIVE POSTERS
-    //SQSP.$objects.postersItems.each(function(i) {
-    //
-    //    var $thisPoster = jQuery(this);
-    //
-    //    SQSP.instances.ParallaxPosters.push( new ParallaxPoster($thisPoster, {
-    //        amount:5,
-    //        offsets: {
-    //            container: 13,
-    //            layers: [
-    //                0,  // hover-layer
-    //                10, // bg-layer
-    //                14, // model-layer
-    //                18  // copy-layer
-    //            ]
-    //        }
-    //    }) );
-    //
-    //});
+    SQSP.$objects.postersContainer = jQuery('.posters');
+    SQSP.$objects.postersItems = jQuery('.prllxposter');
+    SQSP.instances.ParallaxPosters = [];
+
+    // CREATE INTERACTIVE POSTERS
+    SQSP.$objects.postersItems.each(function(i) {
+
+        var $thisPoster = jQuery(this);
+
+        SQSP.instances.ParallaxPosters.push( new ParallaxPoster($thisPoster, {
+            amount:5,
+            offsets: {
+                container: 13,
+                layers: [
+                    0,  // hover-layer
+                    10, // bg-layer
+                    14, // model-layer
+                    18  // copy-layer
+                ]
+            }
+        }) );
+
+    });
 
 };
