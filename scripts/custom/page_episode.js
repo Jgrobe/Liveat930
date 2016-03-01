9429f2b74346c2bb89d49e4169291f64d9725b36
WILD <rag@wild.as>
2016-03-01 16:59:32 +0100

    clipgrid episode detail

--- scripts/custom/page_episode.js
+++ scripts/custom/page_episode.js
@@ -6,4 +6,13 @@ SQSP.functions.initPage = function() {
         console.log('Page Episode loaded');
     });
 
+    SQSP.isntances.relatedClipsGrid = new ClipGrid(jQuery('.related-clips-gallery'), {
+        sizes : ['small']
+    });
+
+    SQSP.isntances.relatedClipsCategoriesGrid = new ClipGrid(jQuery('.related-clips-categories-gallery'), {
+        sizes : ['small', 'medium'],
+        distributeSizes : [1,1,0,0,0]
+    });
+
 };
