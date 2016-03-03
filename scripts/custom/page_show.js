93eeab6febcca830ba0f551ac2f2b306bf3e49d3
WILD <rag@wild.as>
2016-03-03 15:50:17 +0100

    page show episode qiery

--- scripts/custom/page_show.js
+++ scripts/custom/page_show.js
@@ -2,7 +2,8 @@ SQSP.functions.initPage = function() {
 
     console.log('init clips overview page');
 
-    SQSP.instances.videoGrid = new ClipGrid(jQuery('.video-gallery-videos'), {
+    var $videoGrid = jQuery('.video-gallery-videos');
+    SQSP.instances.videoGrid = new ClipGrid($videoGrid, {
         itemSelector : '.video',
         sizes : ['medium'],
         onInit:function() {
