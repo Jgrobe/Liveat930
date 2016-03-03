e857128ae609a089e3df94f96ce4b2157910e5d7
WILD <rag@wild.as>
2016-03-03 16:19:22 +0100

    page host gallery

---
+++ scripts/custom/page_host.js
@@ -0,0 +1,11 @@
+SQSP.functions.initPage = function() {
+
+    console.log('init clips overview page');
+
+    if(SQSP.instances.PageGallery !== 'undefined') {
+
+        console.log('init the host mofo',SQSP.instances.PageGallery);
+
+    }// endif
+
+};
