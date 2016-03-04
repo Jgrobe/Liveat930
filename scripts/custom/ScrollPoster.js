69f6a2818d473ee9a6591dab209bb5f235e20f23
WILD <rag@wild.as>
2016-03-04 11:42:42 +0100

    scrollposters hoverstate updates

--- scripts/custom/ScrollPoster.js
+++ scripts/custom/ScrollPoster.js
@@ -33,51 +33,7 @@ var ScrollPoster = function($container, options) {
     };
 
     SP.init = function() {
-        //console.log('poster objects', SP.$object);
-
-        // HOVER LISTENERS
-        //SP.$object.hoverTrigger.on('mouseenter', function(e) {
-        //    var $hovered = jQuery(this);
-        //    if(is_mobile()) return false;
-        //    //if(typeof SP.unHoverIntent !== 'undefined') clearTimeout(SP.hoverIntent);
-        //    //SP.hoverIntent = setTimeout(function() {
-        //        //console.log('SP.$object.hoverLayer.attr', $hovered.attr('data-img'));
-        //        //$hovered.siblings().addClass(SP.options.siblingsHoverClass);
-        //
-        //    SP.$object.container.addClass(SP.options.posterHoverClass);
-        //    $hovered.addClass(SP.options.elementHoverClass);
-        //
-        //        // HOVER BG IMAGE
-        //        //var hoverIMG = new Image();
-        //        //
-        //        //hoverIMG.onload = function() {
-        //            jQuery(SP.$object.hoverFXApplicants).each(function(i, elem) {
-        //                //console.log('--------------------------- this hover applicant', elem);
-        //                jQuery(elem).css({
-        //                    'background-image' : 'url('+ $hovered.attr('data-img') +')'
-        //                }).addClass(SP.options.posterHoverClass);
-        //            });
-        //        //};// img load
-        //        //hoverIMG.src = $hovered.attr('data-img');
-        //        // ITEMS FADE
-        //    //}, 300);
-        //});
-        //SP.$object.unhoverTrigger.on('mouseleave', function(e) {
-        //    var $unhovered = jQuery(this);
-        //    if(is_mobile()) return false;
-        //    //if(typeof SP.hoverIntent !== 'undefined') clearTimeout(SP.hoverIntent);
-        //    //SP.unHoverIntent = setTimeout(function() {
-        //        // ITEMS FADE
-        //        SP.$object.container.removeClass(SP.options.posterHoverClass);
-        //        $unhovered.removeClass(SP.options.elementHoverClass);
-        //        // HOVER BG IMAGE
-        //        jQuery(SP.$object.hoverFXApplicants).each(function(i, elem) {
-        //            jQuery(elem).removeClass(SP.options.posterHoverClass).css({
-        //                'background-image' : ''
-        //            });
-        //        });
-        //    //}, 300);
-        //});
+        console.log('poster objects', SP.$object);
 
         jQuery(document).on('click', '.artist', function(e) {
             e.preventDefault();
@@ -100,34 +56,9 @@ var ScrollPoster = function($container, options) {
             }// endif
         });
 
-        //jQuery(document).on('click', function(e){
-        //    console.log('clicked', e);
-        //    var $clicked = jQuery(e.target);
-        //    console.log('close hoverstate?', $clicked, $clicked.hasClass('artist'));
-        //    if(SP.HOVERSTAT_ACTIVE) {
-        //        e.preventDefault();
-        //        close_hoverstate();
-        //        SQSP.instances.FieldTracker.$object.tracker.removeClass('active');
-        //        TweenMax.to(SQSP.$objects.burger,.3, {autoAlpha:1, clearProps:'autoAlpha'});
-        //    } else {
-        //        if($clicked.hasClass('artist') || $clicked.parent().hasClass('artist')) {
-        //            e.preventDefault();
-        //            open_hoverstate($clicked);
-        //            TweenMax.to(SQSP.$objects.burger,.3, {autoAlpha:0});
-        //            SQSP.instances.FieldTracker.update({x: e.clientX, y: e.clientY});
-        //            SQSP.instances.FieldTracker.$object.tracker.addClass('active');
-        //        }
-        //    }
-        //});
-        // artist click listener
-        //SP.$object.hoverTrigger.on('click', function(e) {
-        //    e.preventDefault();
-        //    var $clicked = jQuery(this);
-        //    console.log('open hoverstate?', SP.HOVERSTAT_ACTIVE);
-        //    if(!SP.HOVERSTAT_ACTIVE) open_hoverstate($clicked);
-        //
-        //});
     };// init()
+
+    if(SP.options.autoInit) SP.init();
 
     function open_hoverstate($clicked) {
 
@@ -140,15 +71,14 @@ var ScrollPoster = function($container, options) {
             var $thisApplicant = jQuery(this);
             $thisApplicant.css({
                 'background-image' : 'url('+ $clicked.attr('data-img') +')'
-            }).addClass(SP.options.posterHoverClass);
+            });
 
             if(SP.options.gradientMap) {
-                var gradmap = GradientMaps.applyGradientMap($thisApplicant.get(0), SP.options.gradientMap);
-                console.log('gradient map' ,gradmap);
+                GradientMaps.applyGradientMap($thisApplicant.get(0), SP.options.gradientMap);
             }
         });
         SP.HOVERSTAT_ACTIVE = true;
-    }// open_hoverstate
+    }// open_hoverstate()
 
     function close_hoverstate() {
         jQuery(SP.$object.hoverClassApplicants).each(function(){jQuery(this).removeClass(SP.options.posterHoverClass)});
@@ -166,8 +96,6 @@ var ScrollPoster = function($container, options) {
             //});
         });
         SP.HOVERSTAT_ACTIVE = false;
-    }
-
-    if(SP.options.autoInit) SP.init();
+    }// close_hoverstate()
 
 };// ScrollPoster
