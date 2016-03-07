c0f00582a47d407d9627962480b9a2bed5389d3f
WILD <rag@wild.as>
2016-03-07 19:12:09 +0100

    imagegallerycopycontainer height tweening

--- scripts/custom/ImageGallery.js
+++ scripts/custom/ImageGallery.js
@@ -205,20 +205,17 @@ var ImageGallery = function($container, options) {
     function update_imageCopy() {
         if( !elem_exists(IG.$object.imageCopy) ) return 'null'; // must be tween, timeline, fn or string b/c inserted into timeline
         //console.log('------- update_imageCopy()');
-        var oldHeight = IG.$object.imageCopyContainer.outerHeight();
-        console.log('imageCopycontainer oldheight', oldHeight);
+        var oldHeight = IG.$object.imageCopy.outerHeight();
         var newHeight;
 
         var copyTL = new TimelineMax();
         copyTL.to(IG.$object.imageCopy, IG.options.duration, {autoAlpha:0});
 
         copyTL.add(function() {
-            //IG.$object.imageCopyContainer.height('auto');
             IG.$object.imageCopy.html(IG.options.images[IG.options.currentIndex].copy);
-            newHeight = IG.$object.imageCopyContainer.outerHeight();
-            console.log('imageCopycontainer newheight', newHeight);
+            newHeight = IG.$object.imageCopy.outerHeight();
         });
-        copyTL.fromTo(IG.$object.imageCopyContainer, IG.options.duration *.5, {height:oldHeight}, {height:newHeight, ease:Strong.easeInOut, clearProps:'height', onUpdate:function(){console.log('tweening height', IG.$object.imageCopyContainer.height());}});
+        copyTL.fromTo(IG.$object.imageCopy, IG.options.duration *.5, {height:oldHeight}, {height:newHeight, ease:Strong.easeInOut, clearProps:'height', onUpdate:function(){console.log('tweening height', IG.$object.imageCopyContainer.height());}});
         copyTL.to(IG.$object.imageCopy, IG.options.duration, {autoAlpha:1}, '-='+(IG.options.duration *.25));
 
         return copyTL;
