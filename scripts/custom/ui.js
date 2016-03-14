80b9c6cee1b391b2fb649c916e768fa97c4d2c7b
WILD <rag@wild.as>
2016-03-14 19:06:48 +0100

    video load safety

--- scripts/custom/ui.js
+++ scripts/custom/ui.js
@@ -43,7 +43,7 @@ function init_sqsp() {
     $clipItems.on('mouseenter', function(e) {
         var $hovered = jQuery(this);
         console.log('.video mouseenter', $hovered, e);
-        $hovered.find('.cta-container').mouseenter();
+        $hovered.find('.cta-container').trigger('mouseenter');
     });
     $clipItems.on('mouseleave', function(e) {
         //console.log('.video mouseleave', e);
