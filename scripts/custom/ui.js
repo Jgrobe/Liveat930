61d5a050804586c43b2ce49e82f3fc52279c73ee
WILD <rag@wild.as>
2016-03-15 18:45:59 +0100

    clipables test

--- scripts/custom/ui.js
+++ scripts/custom/ui.js
@@ -114,15 +114,23 @@ function populate_namespaces() {
             TweenMax.set(jQuery('.page'), {autoAlpha:0});
             SQSP.$objects.preloaderClipables = jQuery('.onloadclip');
 
-            //SQSP.$objects.preloaderClipables.each(function(i){
-            //    var $this = jQuery(this);
-            //
-            //});
+
+            SQSP.$objects.preloaderClipables.each(function(i){
+                var $this = jQuery(this);
+
+            });
         },
         onComplete:function(e) {
 
-            var clipHTML = '<svg style="width:0;height:0;"><defs><clipPath id="mask" clipPathUnits="objectBoundingBox"><polygon points="0 0, 1 0, 1 0, 0 0"></polygon></clipPath></defs></svg>';
-            var duration = .75;
+            var initPoints = [
+                {x1:0, y1:0, x2:1, y2:0, x3:1, y3:0, x4:0, y4:0},// top bottom
+                {x1:0, y1:0, x2:0, y2:0, x3:0, y3:1, x4:0, y4:1},// left right
+                {x1:1, y1:0, x2:1, y2:0, x3:1, y3:1, x4:1, y4:1},// right left
+                {x1:0, y1:1, x2:1, y2:1, x3:1, y3:1, x4:0, y4:1}// bottom top
+            ];
+
+            var clipHTML = '<svg style="width:0;height:0;"><defs><clipPath id="mask" clipPathUnits="objectBoundingBox"><polygon points=""></polygon></clipPath></defs></svg>';
+            var duration = .4;
             var tl = new TimelineMax();
             tl.to(jQuery('.page'),(duration*1.5), {autoAlpha:1, onComplete:function() {
                 //jQuery('body').removeClass('hidden');
@@ -133,11 +141,12 @@ function populate_namespaces() {
                 var $this = jQuery(this),
                     $clipSVG = jQuery(clipHTML),
                     maskID = 'mask_'+ i,
-                    polygonPoints = {x1:0,y1:0,x2:1,y2:0,x3:1,y3:0,x4:0,y4:0};
+                    polygonPoints = _.cloneDeep( initPoints[ Math.floor(Math.random()*initPoints.length) ] );
 
                 $clipSVG.find('#mask').attr({id : maskID });
                 $clipSVG.insertAfter($this);
                 var $polygon = $clipSVG.find('polygon');
+                $polygon.attr({points:get_points_string(polygonPoints)['inline']});
                 updateCSS($this, maskID, polygonPoints);
 
 
@@ -145,7 +154,7 @@ function populate_namespaces() {
 
                     console.log('updateing polygon', polygonPoints.y3);
                     $polygon.attr({
-                        points : polygonPoints.x1 +' '+ polygonPoints.y1 +', '+ polygonPoints.x2 +' '+ polygonPoints.y2 +', '+ polygonPoints.x3 +' '+ polygonPoints.y3 +', '+ polygonPoints.x4 +' '+ polygonPoints.y4
+                        points : get_points_string(polygonPoints)['inline']//polygonPoints.x1 +' '+ polygonPoints.y1 +', '+ polygonPoints.x2 +' '+ polygonPoints.y2 +', '+ polygonPoints.x3 +' '+ polygonPoints.y3 +', '+ polygonPoints.x4 +' '+ polygonPoints.y4
                     });
                     updateCSS($this, maskID, polygonPoints);
                 }, onComplete:function($elem, $svg){
@@ -185,10 +194,17 @@ function populate_namespaces() {
                 $elem.css({
                     'overflow' : 'hidden',
                     'clip-path' : 'url(#'+ maskID +')',
-                    '-webkit-clip-path' : 'polygon('+ (polygonPoints.x1*100) +'% '+ (polygonPoints.y1*100) +'%, '+ (polygonPoints.x2*100) +'% '+ (polygonPoints.y2*100) +'%, '+ (polygonPoints.x3*100) +'% '+ (polygonPoints.y3*100) +'%, '+ (polygonPoints.x4*100) +'% '+ (polygonPoints.y4*100) + '%)'
+                    '-webkit-clip-path' : get_points_string(polygonPoints)['css']//'polygon('+ (polygonPoints.x1*100) +'% '+ (polygonPoints.y1*100) +'%, '+ (polygonPoints.x2*100) +'% '+ (polygonPoints.y2*100) +'%, '+ (polygonPoints.x3*100) +'% '+ (polygonPoints.y3*100) +'%, '+ (polygonPoints.x4*100) +'% '+ (polygonPoints.y4*100) + '%)'
                     //'-webkit-clip-path' : 'url(#'+ maskID +')'
                     //'-webkit-clip-path' : 'polygon(% %, % %, % %, %, %)'
                 });
+            }
+
+            function get_points_string(points) {
+                return {
+                    inline : points.x1 +' '+ points.y1 +', '+ points.x2 +' '+ points.y2 +', '+ points.x3 +' '+ points.y3 +', '+ points.x4 +' '+ points.y4,
+                    css : 'polygon('+ (points.x1*100) +'% '+ (points.y1*100) +'%, '+ (points.x2*100) +'% '+ (points.y2*100) +'%, '+ (points.x3*100) +'% '+ (points.y3*100) +'%, '+ (points.x4*100) +'% '+ (points.y4*100) + '%)'
+                }
             }
         }// onComplete()
     });
